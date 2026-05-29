023dd048e5e2287c2066fadc3a455f27cb401538
HPedro <59668414+Huedro@users.noreply.github.com>
2026-05-29 20:07:35 -0300

    basic screen

--- src/app/core/services/gacha.service.ts.service.ts
+++ src/app/core/services/gacha.service.ts.service.ts
@@ -1,9 +1,59 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GachaServiceTsService {
+  constructor() {}
 
-  constructor() { }
+  result = '???';
+  lastTenPulls: string[] = [];
+  ssrPool = ['Modernia', 'Liberalio', 'Cinderella'];
+  srPool = ['Rapi', 'Anis', 'Neon'];
+  rPool = ['Soldier EG', 'iDoll Sun', 'Product 12'];
+
+  pullCount = 0;
+
+  // pullCharacter(isSingle: boolean) {
+  //   this.pullCount++;
+
+  //   const roll = Math.random() * 100;
+
+  //   if (isSingle) {
+  //     this.lastTenPulls = [];
+  //   }
+
+  //   if (roll <= 4) {
+  //     const randomIndex = Math.floor(Math.random() * this.ssrPool.length);
+  //     if (isSingle) {
+  //       this.result = this.ssrPool[randomIndex];
+  //     } else {
+  //       this.lastTenPulls.push(this.ssrPool[randomIndex]);
+  //     }
+  //   } else if (roll <= 47) {
+  //     const randomIndex = Math.floor(Math.random() * this.srPool.length);
+  //     if (isSingle) {
+  //       this.result = this.srPool[randomIndex];
+  //     } else {
+  //       this.lastTenPulls.push(this.srPool[randomIndex]);
+  //     }
+  //   } else {
+  //     const randomIndex = Math.floor(Math.random() * this.rPool.length);
+  //     if (isSingle) {
+  //       this.result = this.rPool[randomIndex];
+  //     } else {
+  //       this.lastTenPulls.push(this.rPool[randomIndex]);
+  //     }
+  //   }
+
+  //   //console.log(this.lastTenPulls);
+  // }
+
+  // pullTenCharacters() {
+  //   this.lastTenPulls = [];
+
+  //   for (let i = 0; i < 10; i++) {
+  //     this.pullCharacter(false);
+  //   }
+  // }
 }
